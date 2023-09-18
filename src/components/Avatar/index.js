e4ba37b063afb7c46d1516fab883f31fb4d7375c
andersons-tuche23 <134718682+andersons-tuche23@users.noreply.github.com>
2023-09-18 18:28:41 -0300

    disney-18-09

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -3,16 +3,21 @@ import Styles from './index.module.scss'
 
 
 
+function Avatar({ onClick}) {
 
 
-function Avatar({onClick}) {
+
+
   return (
     <>
-    <div  className={Styles.cards}>
-      <img onClick={onClick} className={Styles.image} src='https://img.elo7.com.br/product/zoom/278977B/filhote-simba-arquivo-em-camada.jpg' width={100} height={100} />
-      <h1>simba</h1>
-    </div>
-  </>
+      <div className={Styles.cards}>
+        <img onClick={onClick} src='https://img.elo7.com.br/product/zoom/278977B/filhote-simba-arquivo-em-camada.jpg' width={100} height={100} />
+          <h1>simba</h1>
+      </div>
+      <div>
+        <img src=''/>
+      </div>
+    </>
   )
 }
 
